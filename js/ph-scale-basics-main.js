f0f52859f4a05256d49ac709669709c88d75bef9
Chris Malley <cmalley@pixelzoom.com>
2015-07-13 09:22:13 -0600

    add PixelZoom qualifier to credits

--- js/ph-scale-basics-main.js
+++ js/ph-scale-basics-main.js
@@ -19,7 +19,7 @@ define( function( require ) {
   var simOptions = {
     credits: {
       leadDesign: 'Yuen-ying Carpenter',
-      softwareDevelopment: 'Chris Malley',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
       team: 'Julia Chamberlain, Trish Loeblein, Emily B. Moore, Ariel Paul, Katherine Perkins',
       graphicArts: 'Sharon Siman-Tov',
       thanks: 'Conversion of this simulation to HTML5 was funded in part by the Royal Society of Chemistry.'
