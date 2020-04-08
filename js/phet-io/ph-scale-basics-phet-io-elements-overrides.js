938359db8720ae21a054ed26578c3f00d3777b4a
pixelzoom <cmalley@pixelzoom.com>
2020-04-08 16:41:04 -0600

    copy relevant overrides to ph-scale-basics, https://github.com/phetsims/ph-scale/issues/146

--- js/phet-io/ph-scale-basics-phet-io-elements-overrides.js
+++ js/phet-io/ph-scale-basics-phet-io-elements-overrides.js
@@ -1,3 +1,88 @@
-
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "phScaleBasics.global.model.solutes.batteryAcid.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.blood.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.chickenSoup.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.coffee.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.drainCleaner.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.handSoap.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.milk.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.orangeJuice.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.soda.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.spit.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.vomit.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.global.model.solutes.water.nameProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.autoFillEnabledProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.dropper.soluteProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.pHMeter.pHProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.solution.pHProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.solution.soluteVolumeProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.solution.totalVolumeProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.model.solution.waterVolumeProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.beakerNode.tickMarks.tickLabels.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.beakerNode.tickMarks.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.drainFaucetNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.neutralIndicatorNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.pHMeterNode.pHIndicatorNode.numberDisplay.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.pHMeterNode.probeNode.pickableProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.volumeIndicatorNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.waterFaucetNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "phScaleBasics.macroScreen.view.waterFaucetNode.waterLabelNode.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
