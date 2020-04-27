2255c9339d5363a3c07be27412b4a0347c11e583
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:02 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/ph-scale-basics-baseline.js
+++ js/phet-io/ph-scale-basics-baseline.js
@@ -1366,7 +1366,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "phScaleBasics.general.view.simNameProperty": {
+    "phScaleBasics.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
