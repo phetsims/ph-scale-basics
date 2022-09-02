3646665c2c340da4e001b371c6aea5e3ee8175c2
pixelzoom <cmalley@pixelzoom.com>
2022-09-02 09:25:08 -0600

    use tandem name '*Text' consistently for Text nodes, https://github.com/phetsims/ph-scale/issues/241

--- js/ph-scale-basics-phet-io-overrides.js
+++ js/ph-scale-basics-phet-io-overrides.js
@@ -79,7 +79,7 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     "phScaleBasics.macroScreen.view.waterFaucetNode.visibleProperty": {
       "phetioFeatured": true
     },
-    "phScaleBasics.macroScreen.view.waterFaucetNode.waterLabelNode.visibleProperty": {
+    "phScaleBasics.macroScreen.view.waterFaucetNode.waterText.visibleProperty": {
       "phetioFeatured": true
     }
   };
