dc34ec64c300cd0668717cdbb0f061506c9f353a
pixelzoom <cmalley@pixelzoom.com>
2023-05-06 14:01:20 -0600

    partial migration of overrides.js into code, https://github.com/phetsims/ph-scale/issues/284

--- js/ph-scale-basics-phet-io-overrides.js
+++ js/ph-scale-basics-phet-io-overrides.js
@@ -1,15 +1,6 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
   {
-    "phScaleBasics.macroScreen.model.solution.soluteVolumeProperty": {
-      "phetioFeatured": true
-    },
-    "phScaleBasics.macroScreen.model.solution.totalVolumeProperty": {
-      "phetioFeatured": true
-    },
-    "phScaleBasics.macroScreen.model.solution.waterVolumeProperty": {
-      "phetioFeatured": true
-    },
     "phScaleBasics.macroScreen.view.pHMeterNode.pHIndicatorNode.numberDisplay.visibleProperty": {
       "phetioFeatured": true
     }
