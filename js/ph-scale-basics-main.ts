f6a9ebea938a945cff92b6305759c82a8ec3b132
Jesse <jesse.greenberg@colorado.edu>
2025-03-24 13:07:08 -0400

    remove prototype description plugin code from ph-scale-basics, see https://github.com/phetsims/ph-scale/issues/294

--- js/ph-scale-basics-main.ts
+++ js/ph-scale-basics-main.ts
@@ -6,17 +6,12 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import DescriptionContext from '../../joist/js/DescriptionContext.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import PHScaleConstants from '../../ph-scale/js/common/PHScaleConstants.js';
 import PHScaleQueryParameters from '../../ph-scale/js/common/PHScaleQueryParameters.js';
 import MacroScreen from '../../ph-scale/js/macro/MacroScreen.js';
-import Alerter from '../../scenery-phet/js/accessibility/describers/Alerter.js';
 import Tandem from '../../tandem/js/Tandem.js';
-import PHScaleBasicsDescriptionLogic from './description/ph-scale-basics-description-logic.js'; // eslint-disable-line phet/default-import-match-filename
-import PHScaleBasicsDescriptionStrings_en from './description/ph-scale-basics-description-strings_en.js'; // eslint-disable-line phet/default-import-match-filename
-import PHScaleBasicsDescriptionStrings_es from './description/ph-scale-basics-description-strings_es.js'; // eslint-disable-line phet/default-import-match-filename
 import PhScaleBasicsStrings from './PhScaleBasicsStrings.js';
 
 // If autofill query parameter was not in the URL, change the default.
@@ -25,19 +20,6 @@ if ( !QueryStringMachine.containsKey( 'autofill' ) ) {
 }
 
 simLauncher.launch( () => {
-
-  //TODO https://github.com/phetsims/ph-scale/issues/294
-  if ( phet.chipper.queryParameters.supportsDescriptionPlugin ) {
-    PHScaleBasicsDescriptionStrings_en();
-    PHScaleBasicsDescriptionStrings_es();
-    PHScaleBasicsDescriptionLogic();
-
-    phet.log && phet.log( Alerter );
-
-    phet.log && phet.log( PHScaleBasicsDescriptionStrings_en.toString() );
-    phet.log && phet.log( PHScaleBasicsDescriptionLogic.toString() );
-  }
-
   const screens = [
     new MacroScreen( Tandem.ROOT.createTandem( 'macroScreen' ) )
   ];
@@ -45,11 +27,6 @@ simLauncher.launch( () => {
   const sim = new Sim( PhScaleBasicsStrings[ 'ph-scale-basics' ].titleStringProperty, screens, {
     credits: PHScaleConstants.CREDITS,
     phetioDesigned: true
-  } );
-
-  //TODO https://github.com/phetsims/ph-scale/issues/294
-  phet.chipper.queryParameters.supportsDescriptionPlugin && sim.isConstructionCompleteProperty.lazyLink( isConstructionComplete => {
-    DescriptionContext.startupComplete();
   } );
 
   sim.start();
